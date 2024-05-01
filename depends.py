5eb5643f6750273f6594547cf53141f7ac8263f3
Magistrat <nisraton@gmail.com>
2024-05-02 00:00:41 +0300

    [feature] add mongo connect to dependency

---
+++ depends.py
@@ -0,0 +1,23 @@
+from fastapi import Depends
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from settings import MONGO_CONNECTION_PATH
+from settings import MONGO_COLLECTION_SHOP
+
+
+async def get_async_mongo_client_by_motor() -> AsyncIOMotorClient:
+    """
+    Возвращает асинхронный клиент к MongoDB через библиотеку motor
+    :return: Асинхронный клиент к MongoDB
+    """
+    return AsyncIOMotorClient(MONGO_CONNECTION_PATH)
+
+
+async def get_async_mongo_connect_to_shop_by_motor(
+        mongo_client: AsyncIOMotorClient = Depends(get_async_mongo_client_by_motor)
+):
+    """
+    Возвращает асинхронное подключение к MongoDB в коллекцию SHOP через библиотеку motor
+    :return:
+    """
+    return mongo_client[MONGO_COLLECTION_SHOP]
