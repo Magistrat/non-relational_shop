687dccc29634f715903110ebd655b1b233d11c9a
Magistrat <nisraton@gmail.com>
2024-05-01 23:15:01 +0300

    [feature] add endpoint for add item

--- routers/api/shop.py
+++ routers/api/shop.py
@@ -1,7 +1,23 @@
 from fastapi import APIRouter
+from fastapi import status
+
+from schemas import ItemSchema
 
 
 shop_router = APIRouter(
     prefix='/shop',
     tags=['Backend shop API']
 )
+
+
+@shop_router.post(
+    path='/item',
+    summary='Добавление товара в магазин',
+    status_code=status.HTTP_200_OK,
+)
+async def add_item_to_shop(
+    request_body: ItemSchema,
+):
+    """
+    Добавление товара в магазин
+    """
