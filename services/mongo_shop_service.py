725fe83ed23e8531530f42826b1221d961407fa7
Magistrat <nisraton@gmail.com>
2024-05-20 21:45:40 +0300

    [feature] add method for get all items from mongodb

--- services/mongo_shop_service.py
+++ services/mongo_shop_service.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
 from schemas import ItemSchema
@@ -17,3 +19,18 @@ class MongoShopService:
         :return:
         """
         await self._mongo_connect.records.insert_one(dict(shop_item))
+
+    async def get_all_items_in_shop(self) -> List[ItemSchema]:
+        """
+        Получение всех товаров из MongoDB
+        :return:
+        """
+        cursor = self._mongo_connect.records.find({})
+        result = []
+
+        for shop_item in await cursor.to_list(length=100):
+            del shop_item['_id']
+            # shop_item['_id'] = str(shop_item['_id'])
+            result.append(shop_item)
+
+        return result
