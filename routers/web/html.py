55391a1a1b5ec4fff0d7f5550be813e5447fbed3
Magistrat <nisraton@gmail.com>
2024-05-21 22:25:55 +0300

    [feature] shop items in page

--- routers/web/html.py
+++ routers/web/html.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
+from fastapi import Depends
 from fastapi import Request
 from fastapi import status
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from depends import get_mongo_shop_service
+from services import MongoShopService
 
 templates = Jinja2Templates(directory='templates')
 
@@ -18,13 +21,19 @@ html_router = APIRouter(
     status_code=status.HTTP_200_OK,
     response_class=HTMLResponse
 )
-async def get_main_page(request: Request):
+async def get_main_page(
+        request: Request,
+        mongo_shop_service: MongoShopService = Depends(get_mongo_shop_service),
+):
     """
     Главная страница магазина
     """
+    shop_items = await mongo_shop_service.get_all_items_in_shop()
+
     return templates.TemplateResponse(
         'main_page.html',
         {
             'request': request,
+            'shop_items': shop_items
         }
     )
